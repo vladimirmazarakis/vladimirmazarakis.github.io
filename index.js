6f5f72bac273b90341b21562800eb4a044191240
vladimirmazarakis <87600021+vladimirmazarakis@users.noreply.github.com>
2022-09-27 16:20:49 +0300

    Empty fields fix.

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function onFibonacciFormSubmit(event){
     let numberOneField = parseInt(event.target.querySelector('#number-one').value);
     let numberTwoField = parseInt(event.target.querySelector('#number-two').value);
     let count = parseInt(event.target.querySelector('#count').value);
+    if(!numberOneField || !numberTwoField || !count){
+        return;
+    }
     let fibonacciArray = fibonacci(numberOneField, numberTwoField, count);
     let fibonacciBlocksContainer = document.querySelector('.fibonacci-block__blocks__row');
     fibonacciBlocksContainer.innerHTML = '';
